d865dc5d0582272d207e08ae06a1641818033964
leighton-tracksuit <165849383+leighton-tracksuit@users.noreply.github.com>
2024-09-23 15:38:33 +1200

    FEATURE: VALUE-4995 - update the funnel_metrics table to reflect the correct api response values

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import json
 import sys
 
 from api import Api
-from data_classes import FunnelMetrics
 
 sys.path.append('sdk_pb2')
 
@@ -43,18 +42,20 @@ class ConnectorService(connector_sdk_pb2_grpc.ConnectorServicer):
         t1 = table_list.tables.add(name="funnel_metrics")
         t1.columns.add(name="id", type=common_pb2.DataType.INT, primary_key=True)
         t1.columns.add(name="account_brand_id", type=common_pb2.DataType.INT)
-        t1.columns.add(name="wave_date", type=common_pb2.DataType.STRING)
         t1.columns.add(name="brand_id", type=common_pb2.DataType.INT)
         t1.columns.add(name="brand_name", type=common_pb2.DataType.STRING)
-        t1.columns.add(name="is_account_brand", type=common_pb2.DataType.STRING)
+        t1.columns.add(name="filter", type=common_pb2.DataType.STRING)
+        t1.columns.add(name="filter_type", type=common_pb2.DataType.STRING)
+        t1.columns.add(name="wave_date", type=common_pb2.DataType.STRING)
         t1.columns.add(name="question_type", type=common_pb2.DataType.STRING)
-        t1.columns.add(name="percentage", type=common_pb2.DataType.DOUBLE)
-        t1.columns.add(name="population", type=common_pb2.DataType.INT)
         t1.columns.add(name="category_id", type=common_pb2.DataType.INT)
         t1.columns.add(name="category_name", type=common_pb2.DataType.STRING)
         t1.columns.add(name="geography_id", type=common_pb2.DataType.INT)
         t1.columns.add(name="geography_name", type=common_pb2.DataType.STRING)
-        t1.columns.add(name="sample_size_quality", type=common_pb2.DataType.STRING)
+        t1.columns.add(name="base", type=common_pb2.DataType.STRING)
+        t1.columns.add(name="weight", type=common_pb2.DataType.DOUBLE)
+        t1.columns.add(name="base_weight", type=common_pb2.DataType.DOUBLE)
+        t1.columns.add(name="percentage", type=common_pb2.DataType.DOUBLE)
 
         return connector_sdk_pb2.SchemaResponse(without_schema=table_list)
 
@@ -75,44 +76,21 @@ class ConnectorService(connector_sdk_pb2_grpc.ConnectorServicer):
 
         if True:
             operation = connector_sdk_pb2.Operation()
-            funnel_metrics = [
-                FunnelMetrics(
-                    account_brand_id=1,
-                    wave_date="2024-06-01",
-                    brand_id=101,
-                    brand_name="Brand1",
-                    is_account_brand="True",
-                    question_type="UNPROMPTED_AWARENESS",
-                    percentage=50.0,
-                    population=1000,
-                    category_id=1,
-                    category_name="Category1",
-                    geography_id=1,
-                    geography_name="Geography1",
-                    sample_size_quality="High"
-                )
-            ]
+            funnel_metrics = Api(jwt_token).get_funnel_metrics_for_initial_sync("2024-04-01", "2024-06-01")
 
-            index = 1
             for metric in funnel_metrics:
                 print(f"Upserting record {metric.brand_name}")
                 record = connector_sdk_pb2.Record()
                 record.type = common_pb2.OpType.UPSERT
                 record.table_name = "funnel_metrics"
 
-                # TODO: ID should come from the data source
                 val_id = common_pb2.ValueType()
-                val_id.int = index
-                index += 1
+                val_id.int = metric.id
                 record.data["id"].CopyFrom(val_id)
 
                 val_account_brand_id = common_pb2.ValueType()
                 val_account_brand_id.int = metric.account_brand_id
                 record.data["account_brand_id"].CopyFrom(val_account_brand_id)
-
-                val_wave_date = common_pb2.ValueType()
-                val_wave_date.string = metric.wave_date
-                record.data["wave_date"].CopyFrom(val_wave_date)
 
                 val_brand_id = common_pb2.ValueType()
                 val_brand_id.int = metric.brand_id
@@ -122,42 +100,49 @@ class ConnectorService(connector_sdk_pb2_grpc.ConnectorServicer):
                 val_brand_name.string = metric.brand_name
                 record.data["brand_name"].CopyFrom(val_brand_name)
 
-                # TODO: is_account_brand is a boolean, how do I represent it?
-                val_is_account_brand = common_pb2.ValueType()
-                val_is_account_brand.string = metric.is_account_brand
-                record.data["is_account_brand"].CopyFrom(val_is_account_brand)
+                val_filter = common_pb2.ValueType()
+                val_filter.string = metric.filter
+                record.data["filter"].CopyFrom(val_filter)
 
-                val_question_type = common_pb2.ValueType()
-                val_question_type.string = metric.question_type
-                record.data["question_type"].CopyFrom(val_question_type)
+                val_filter_type = common_pb2.ValueType()
+                val_filter_type.string = metric.filter_type
+                record.data["filter_type"].CopyFrom(val_filter_type)
 
-                val_percentage = common_pb2.ValueType()
-                val_percentage.double = metric.percentage
-                record.data["percentage"].CopyFrom(val_percentage)
+                val_wave_date = common_pb2.ValueType()
+                val_wave_date.string = metric.wave_date
+                record.data["wave_date"].CopyFrom(val_wave_date)
 
                 val_population = common_pb2.ValueType()
                 val_population.int = metric.population
                 record.data["population"].CopyFrom(val_population)
 
-                val_category_id = common_pb2.ValueType()
-                val_category_id.int = metric.category_id
-                record.data["category_id"].CopyFrom(val_category_id)
+                val_category = common_pb2.ValueType()
+                val_category.int = metric.category
+                record.data["category"].CopyFrom(val_category)
 
-                val_category_name = common_pb2.ValueType()
-                val_category_name.string = metric.category_name
-                record.data["category_name"].CopyFrom(val_category_name)
+                val_geography = common_pb2.ValueType()
+                val_geography.int = metric.geography
+                record.data["geography"].CopyFrom(val_geography)
 
-                val_geography_id = common_pb2.ValueType()
-                val_geography_id.int = metric.geography_id
-                record.data["geography_id"].CopyFrom(val_geography_id)
+                val_base = common_pb2.ValueType()
+                val_base.string = metric.base
+                record.data["base"].CopyFrom(val_base)
 
-                val_geography_name = common_pb2.ValueType()
-                val_geography_name.string = metric.geography_name
-                record.data["geography_name"].CopyFrom(val_geography_name)
+                val_weight = common_pb2.ValueType()
+                val_weight.double = metric.weight
+                record.data["weight"].CopyFrom(val_weight)
 
-                val_sample_size_quality = common_pb2.ValueType()
-                val_sample_size_quality.string = metric.sample_size_quality
-                record.data["sample_size_quality"].CopyFrom(val_sample_size_quality)
+                val_base_weight = common_pb2.ValueType()
+                val_base_weight.double = metric.base_weight
+                record.data["base_weight"].CopyFrom(val_base_weight)
+
+                val_percentage = common_pb2.ValueType()
+                val_percentage.double = metric.percentage
+                record.data["percentage"].CopyFrom(val_percentage)
+
+                val_question_type = common_pb2.ValueType()
+                val_question_type.string = metric.question_type
+                record.data["question_type"].CopyFrom(val_question_type)
 
                 operation.record.CopyFrom(record)
                 print("Upserting now record")
@@ -166,6 +151,7 @@ class ConnectorService(connector_sdk_pb2_grpc.ConnectorServicer):
         log = connector_sdk_pb2.LogEntry()
         log.level = connector_sdk_pb2.LogLevel.INFO
         yield connector_sdk_pb2.UpdateResponse(log_entry=log)
+
 
 def start_server():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
